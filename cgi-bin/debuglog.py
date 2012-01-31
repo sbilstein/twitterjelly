35d8f140dbc28e416424c3cee3d2831ab951d3eb
Jonathan Pezzino <jonathanpezzino2011@u.northwestern.edu>
2012-01-31 03:31:39 +0000

    path name herp derp

--- cgi-bin/debuglog.py
+++ cgi-bin/debuglog.py
@@ -6,10 +6,10 @@ def msg(*args, mode="server"):
     if mode == "debug":
         print(*args)
     elif mode =="server":
-        print(*args, file=open("../../log/debug.log", "a"))
+        print(*args, file=open("../../logs/debug.log", "a"))
 
 def pprint_msg(obj, mode="server"):
     if mode == "debug":
         pprint.pprint(obj)
     elif mode == "server":
-        print(pprint.pformat(obj), file=open("../../log/debug.log", "a"))
+        print(pprint.pformat(obj), file=open("../../logs/debug.log", "a"))
