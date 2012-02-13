4a81f5bc92e974c53fde5e282ff1c63a51caa1c7
Jonathan Pezzino <jpez@u.northwestern.edu>
2012-02-12 23:51:31 -0800

    Environment-sensitive configuration scripts.
    Functions to add and delete celebrities.

--- cgi-bin/configure_os.py
+++ cgi-bin/configure_os.py
@@ -1,1 +1,57 @@
 __author__ = 'jonathan'
+from optparse import OptionParser
+import os
+
+environment_shebangs = {}
+
+windows_aliases = ['windows', 'nt']
+nix_aliases =  ['posix', 'mac', 'osx', 'ubuntu', 'linux', 'nix']
+
+for windows_alias in windows_aliases:
+    environment_shebangs[windows_alias] = 'C:\Python32\python.exe'
+
+for nix_alias in nix_aliases:
+    environment_shebangs[nix_alias] = '/usr/bin/env python'
+
+parser = OptionParser()
+parser.add_option("-e", "--environment", action="store", dest="environment", help="The environment for which to configure.")
+parser.add_option("-f", "--file", action="store", dest="filename", help="The file to configure for the chosen environment.")
+(options, args) = parser.parse_args()
+
+environment = options.environment
+filename=options.filename
+
+if not environment:
+    environment = os.name
+
+if filename and environment:
+    if environment in nix_aliases:
+        destination_environment = windows_aliases[0]
+    else:
+        destination_environment = nix_aliases[0]
+
+    read_original_file = open(filename, 'r')
+    original_contents = [line.replace(environment_shebangs[environment], environment_shebangs[destination_environment])
+                         for line in read_original_file.readlines()]
+    read_original_file.close()
+
+    configured_file_contents = [line.replace(environment_shebangs[destination_environment], environment_shebangs[environment])
+                                for line in original_contents]
+
+    original_file_backup = open(filename+'.bak','w')
+    for line in original_contents:
+        original_file_backup.write(line)
+    original_file_backup.close()
+
+    write_original_file = open(filename,'w')
+    write_original_file.write(''.join(configured_file_contents))
+    write_original_file.close()
+
+    print("configured %s to %s"%(filename, environment))
+
+else:
+    print("fail!")
+
+
+
+
--- cgi-bin/configure_windows.bat
+++ cgi-bin/configure_windows.bat
@@ -1,2 +1,3 @@
-python configure_os.py -e nt -f .\GetBitLink.py
-python configure_os.py -e nt -f .\GetBitLink.py
+python configure_os.py -e nt -f ./GetBitlyLink.py
+python configure_os.py -e nt -f ./GetCelebMatchesJSON.py
+python configure_os.py -e nt -f ./StoredResult.py
