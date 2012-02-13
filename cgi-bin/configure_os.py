40b9e5a0f09da7b73c85d09fd407b9b70dace9ac
Jonathan Pezzino <jpez@u.northwestern.edu>
2012-02-13 00:14:43 -0800

    This is why you never program after inhaling the loolaf.

--- cgi-bin/configure_os.py
+++ cgi-bin/configure_os.py
@@ -26,16 +26,16 @@ if not environment:
 
 if filename and environment:
     if environment in nix_aliases:
-        destination_environment = windows_aliases[0]
+        from_environment = windows_aliases[0]
     else:
-        destination_environment = nix_aliases[0]
+        from_environment = nix_aliases[0]
 
     read_original_file = open(filename, 'r')
-    original_contents = [line.replace(environment_shebangs[environment], environment_shebangs[destination_environment])
+    original_contents = [line.replace(environment_shebangs[from_environment], environment_shebangs[environment])
                          for line in read_original_file.readlines()]
     read_original_file.close()
 
-    configured_file_contents = [line.replace(environment_shebangs[environment], environment_shebangs[destination_environment])
+    configured_file_contents = [line.replace(environment_shebangs[from_environment], environment_shebangs[environment])
                                 for line in original_contents]
 
     original_file_backup = open(filename+'.bak','w')
@@ -47,7 +47,7 @@ if filename and environment:
     write_original_file.write(''.join(configured_file_contents))
     write_original_file.close()
 
-    print("configured %s to %s"%(filename, destination_environment))
+    print("configured %s to %s"%(filename, environment))
 
 else:
     print("fail!")
