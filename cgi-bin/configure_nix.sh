f3c9c0d5affb214c8673076270445e797d2ab90e
Jonathan Pezzino <jpez@u.northwestern.edu>
2012-02-13 00:05:52 -0800

    ffffffffffffffffffuuuuuuuuuuuuuu

--- cgi-bin/configure_nix.sh
+++ cgi-bin/configure_nix.sh
@@ -1,3 +1,3 @@
-python ~/lemurturtle/cgi-bin/configure_os.py -e nt -f ./GetBitlyLink.py
-python ~/lemurturtle/cgi-bin/configure_os.py -e nt -f ./GetCelebMatchesJSON.py
-python ~/lemurturtle/cgi-bin/configure_os.py -e nt -f ./GetStoredResult.py
+python ~/lemurturtle/cgi-bin/configure_os.py -e nt -f ~/lemurturtle/cgi-bin/GetBitlyLink.py
+python ~/lemurturtle/cgi-bin/configure_os.py -e nt -f ~/lemurturtle/cgi-bin/GetCelebMatchesJSON.py
+python ~/lemurturtle/cgi-bin/configure_os.py -e nt -f ~/lemurturtle/cgi-bin/GetStoredResult.py
