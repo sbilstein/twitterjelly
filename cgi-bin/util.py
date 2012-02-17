cdc47900359a6ffad90837e0651c5fbd7b913c49
Jonathan Pezzino <jpez@u.northwestern.edu>
2012-02-16 19:01:22 -0800

    Add authentication to urllib calls.
    [Ignore server configurations, our deployment scripts handle this stuff autmatically now.]

--- cgi-bin/util.py
+++ cgi-bin/util.py
@@ -2,6 +2,7 @@ from _ctypes_test import func
 from functools import wraps
 import time
 import debuglog
+import urllib.request
 
 def perftest(func):
     """A decorator that prints the time a function takes to execute."""
@@ -15,3 +16,23 @@ def perftest(func):
         return value
         # Now a few lines needed to make simple_decorator itself
     return wrapper
+
+# create a password manager
+password_mgr = urllib.request.HTTPPasswordMgrWithDefaultRealm()
+
+# Add the username and password.
+# If we knew the realm, we could use it instead of None.
+top_level_url = "http://50.56.221.228/"
+password_mgr.add_password(None, top_level_url, "test_swag", "h3ndricks")
+
+handler = urllib.request.HTTPBasicAuthHandler(password_mgr)
+
+# create "opener" (OpenerDirector instance)
+opener = urllib.request.build_opener(handler)
+
+# use the opener to fetch a URL
+opener.open("http://50.56.221.228/cgi-bin/idf.php")
+
+# Install the opener.
+# Now all calls to urllib.request.urlopen use our opener.
+urllib.request.install_opener(opener)
